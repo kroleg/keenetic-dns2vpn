726621fef50bd4323b938885c4194c53eb2cc82d
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2025-05-11 21:19:12 +0300

    log to shared by default

--- dns-proxy/src/config.ts
+++ dns-proxy/src/config.ts
@@ -5,6 +5,6 @@ export const defaultConfig = {
     { host: '192.168.1.1', port: 53 },
     // { host: '8.8.4.4', port: 53 }
   ],
-  logResolvedToFile: process.env.LOG_RESOLVED_TO_FILE || 'dns-proxy.log',
+  logResolvedToFile: process.env.LOG_RESOLVED_TO_FILE || '../shared-logs/dns-proxy.log',
   // timeout: 5000,
 };
